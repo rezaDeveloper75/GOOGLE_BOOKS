91ecbc9d6640b806e4b9299e27eef6e40756f483
Reza <rezadeveloper75@gmail.com>
2023-03-09 15:39:51 +0330

    Search Section is added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/navbar';
+import SearchSection from './components/searchSection/searchSection';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SearchSection/>
     </div>
   );
 }
--- src/components/searchSection/searchSection.jsx
+++ src/components/searchSection/searchSection.jsx
@@ -2,7 +2,23 @@ import React, { Component } from 'react';
 import './searchSection.css';
 
 const SearchSection = () => {
-    return ( <></> );
+    return ( 
+    <div className="search">
+        <div className="s-left">
+            <span>Today a reader</span>
+            <span>Tomorrow a leader</span>
+        </div>
+        <div className="s-right">
+            <span>An open book shop that you can access anywhere, anytime</span>
+            <div className='s-holder'>
+                <input className='s-box' type="text" placeholder='Type Somethings ...'/>
+                <button className='s-btn'>
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <span>Search</span>
+                </button>
+            </div>
+        </div>
+    </div> );
 }
  
 export default SearchSection;
